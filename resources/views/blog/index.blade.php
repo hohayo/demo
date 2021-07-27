112461e9d1e4082aad5e26700e1d73e029e51fb3
Kyle Chuang <hohayo@gmail.com>
2021-07-28 01:18:11 +0800

    feat: Add auth check for create post

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -8,26 +8,38 @@
             </h1>
         </div>
     </div>
-    
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b">
-        <div>
-            <img src="https://cdn.pixabay.com/photo/2015/02/02/11/09/office-620822_960_720.jpg" alt="" width="700">
-        </div>
-        <div>
-            <h2 class="text-gray-700 font-bold text-5xl pb-4">
-                Learn how to write Laravel code    
-            </h2>
-            <span class="text-gray-500">
-                By <span class="font-bold italic text-gray-800">
-                    Code With Kyle
-                </span>, 1 day ago
-            </span>
-            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At sed repudiandae aliquid minima, ad veritatis facilis laboriosam nesciunt a sapiente hic laborum quia tempora iusto nostrum consequuntur dignissimos nemo molestiae.
-            </p>
-            <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-                Keep Reading
+
+    @if (Auth::check())
+        <div class="pt-15 w-4/5 m-auto">
+            <a
+                href="/blog/create"
+                class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                Create Post
             </a>
         </div>
-    </div>
+    @endif
+
+    @foreach($posts as $post)
+        <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b">
+            <div>
+                <img src="{{ $post->image_path }}" alt="" width="700">
+            </div>
+            <div>
+                <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                    {{ $post->title }}    
+                </h2>
+                <span class="text-gray-500">
+                    By <span class="font-bold italic text-gray-800">
+                        {{ $post->user->name }}
+                    </span>, Created on {{ date('jS M Y', strtotime($post->updated_at))}}
+                </span>
+                <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                    {{ $post->description }}
+                </p>
+                <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                    Keep Reading
+                </a>
+            </div>
+        </div>
+    @endforeach
 @endsection
